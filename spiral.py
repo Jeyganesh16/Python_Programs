cf2ea05811294464be4c56850befa781701709b1
subra <subra@e-serbadkgroup.com>
2025-10-27 19:38:25 +0530

    spiral program color changed

--- spiral.py
+++ spiral.py
@@ -2,7 +2,7 @@ import turtle
 spiral = turtle.Turtle()
 spiral.speed(0) 
 spiral.pensize(2)
-spiral.color("purple")
+spiral.color("orange")
 radius = 10
 for i in range(60):
     spiral.circle(radius + i) 
